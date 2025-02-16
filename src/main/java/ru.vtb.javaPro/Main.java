21f4a74a55513a710f2520fdd2234db48372877b
evgengit2007 <vend111@yandex.ru>
2025-02-17 02:42:25 +0700

    Промежуточный коммит

--- src/main/java/ru.vtb.javaPro/Main.java
+++ src/main/java/ru.vtb.javaPro/Main.java
@@ -1,7 +1,10 @@
 package ru.vtb.javaPro;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
     }
 }
